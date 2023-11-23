7b179375793831e4307955ebe0f74050726345db
Luis Puig <lpuig89@gmail.com>
2023-11-23 22:43:09 +0100

    Finish some test

--- src/services/db.connect.ts
+++ src/services/db.connect.ts
@@ -4,8 +4,9 @@ import 'dotenv/config';
 export const dbConnect = () => {
   const user = process.env.USER_DB;
   const passwd = process.env.PASSWD_DB;
-  const cluster = 'cluster0.p2bwofa.mongodb.net';
-  const dataBase = 'Curso_2023_Q4';
+  const cluster = 'cluster0.oyki02c.mongodb.net';
+  const dataBase = 'challenge';
   const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${dataBase}?retryWrites=true&w=majority`;
+
   return mongoose.connect(uri);
 };
